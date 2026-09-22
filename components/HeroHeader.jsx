f9ea039632c7ab1735abb8d0b870e13e802d60f1
shahmeerdeveloperofficial-svg <shahmeerdeveloper.official@gmail.com>
2026-09-22 12:01:52 +0500

    feat: replace hero banner student images with wide 16:9 official school uniform photography

--- components/HeroHeader.jsx
+++ components/HeroHeader.jsx
@@ -6,12 +6,9 @@ import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 const heroImages = [
-  "/tea_uniform_boy.png",
   "/tea_student1.jpg",
   "/tea_student2.jpg",
   "/tea_student3.jpg",
-  "/tea_student4.jpg",
-  "/tea_student5.jpg",
 ];
 
 const HeroHeader = ({ title, description }) => {
